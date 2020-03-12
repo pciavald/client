ad981aa27c2e6a1a79e78b09cb9d86ce89630f64
Daniel Ayoub <danny.a95@gmail.com>
2020-03-12 17:51:31 -0400

    WIP view

--- shared/teams/common/index.tsx
+++ shared/teams/common/index.tsx
@@ -1,1 +1,4 @@
 export {default as Activity, ModalTitle} from './activity'
+export {default as ChannelsWidget} from './channels-widget'
+export {useChannelMeta, useAllChannelMetas} from './channel-hooks'
+export {default as SelectionPopup} from './selection-popup'
